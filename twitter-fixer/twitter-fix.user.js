b40c5ce62b2e3f51c85a62d5a6e23f5448b1cc0e
Chris Bannister <c.bannister@gmail.com>
2012-03-11 12:11:08 +0000

    Register the window onhashchange to reclean when we change tabs.

--- twitter-fixer/twitter-fix.user.js
+++ twitter-fixer/twitter-fix.user.js
@@ -4,14 +4,39 @@
 // ==/UserScript==
 
 // Bceause new twitter only uses 50% of my screen, which is useless
-var container = document.getElementById("page-container");
 
-var dash = container.getElementsByClassName("dashboard")[0];
-var content = container.getElementsByClassName("content-main")[0];
+function init() {
+    window.onhashchange = setup;
 
-dash.style.float = "right";
-content.style.float = "left";
+    clean();
+}
 
-container.style.width = "60%";
-content.style.width = "70%";
-dash.style.width = "29%";
+function clean() {
+    console.log("Clean = " + window.location);
+    var container = document.getElementById("page-container");
+
+    var dash = container.getElementsByClassName("dashboard")[0];
+    var content = container.getElementsByClassName("content-main")[0];
+
+    if(dash == undefined) {
+        return;
+    }
+
+    dash.style.float = "right";
+    content.style.float = "left";
+
+    container.style.width = "60%";
+    content.style.width = "70%";
+    dash.style.width = "29%";
+}
+
+function setup() {
+    console.log("Setup state = " + document.readyState);
+    if(document.readyState == "complete") {
+        init();
+    } else {
+        window.onload = init;
+    }
+}
+
+setup();
